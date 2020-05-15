4f8800a23053ec5c44dc408fc60885b92ee61bcf
JoaoMarcosBritoSantos <65055414+JoaoMarcosBritoSantos@users.noreply.github.com>
2020-05-15 20:00:01 -0300

    Cabecalho

--- src/aplicacao/programa.java
+++ src/aplicacao/programa.java
@@ -5,6 +5,28 @@ import java.util.Scanner;
 
 public class programa {
 public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	
+	int  quantidadedecompras, atraso;
+	char maioriadascompras;
+	double ticket;
+	
+	System.out.println("SISTEMA DE PERFIL DO CLIENTE");
+	System.out.println("--------------------------------");
+	System.out.println("INFORME OS DADOS DO ÚLTIMO ANO");
+	System.out.println(" ");
+	System.out.printf("Quantas compras o cliente fez no último ano? ");
+	quantidadedecompras = sc.nextInt();
+	System.out.printf("Qual o ticket médio? ");
+	ticket = sc.nextDouble();
+	System.out.println(" ");
+	System.out.printf("Quantas vezes o cliente atrasou o pagamento? ");
+	atraso = sc.nextInt();
+	System.out.printf("A maioria da compras foi em dinheiro, cartão, ou boleto (D,C,B)? ");
+	maioriadascompras = sc.next().charAt(0);
+	System.out.println(" ");
+
+
 
 		sc.close();
 	}
